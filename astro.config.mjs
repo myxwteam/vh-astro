0541f9fbfa39e85df1cbc8fdef263cd8998131aa
xwteam <xwteam@xwteam.cn>
2025-11-27 11:39:26 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-11-27 09:04:55
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-11-27 11:28:14
+ * @LastEditTime: 2025-11-27 11:37:18
  * @FilePath: \vh-astro\astro.config.mjs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,8 +27,6 @@ import swup from '@swup/astro';
 // https://astro.build/config
 export default defineConfig({
 	site: SITE_INFO.Site,
-	output: 'server',
-	adapter: cloudflare(),
 	build: { assets: 'vh_static' },
 	integrations: [swup({
 		theme: false,
