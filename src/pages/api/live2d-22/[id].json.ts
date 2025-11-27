80f30ae836a2c631e390584fd9c8f551d9821872
xwteam <xwteam@xwteam.cn>
2025-11-27 11:20:49 +0800

    Add files via upload

--- src/pages/api/live2d-22/[id].json.ts
+++ src/pages/api/live2d-22/[id].json.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from 'astro';
 
+export const prerender = false;
+
 // 模型列表配置
 const modelList: Record<string, (string | string[])[]> = {
   "default.v2": ["texture_01.png", "texture_02.png", "texture_03.png"],
